639aa568fd91c708bec77b95228e482e6ca253e3
Luca Silverentand <luca@onezero.company>
2025-09-02 01:21:12 +0200

    test: add core and plugin tests (#16)

---
+++ plugins/brew/src/__tests__/brew.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "bun:test";
+import { tap, formula, cask, plugin } from "..";
+
+describe("brew functions", () => {
+  test("creates tap", () => {
+    expect(tap("homebrew/cask")).toEqual({
+      plugin: "brew",
+      kind: "tap",
+      id: "homebrew/cask",
+    });
+  });
+
+  test("creates formula", () => {
+    expect(formula({ id: "node" })).toEqual({
+      plugin: "brew",
+      kind: "formula",
+      id: "node",
+    });
+  });
+
+  test("creates cask", () => {
+    expect(cask("google-chrome")).toEqual({
+      plugin: "brew",
+      kind: "cask",
+      id: "google-chrome",
+    });
+  });
+
+  test("validates with plugin schema", () => {
+    const item = formula("bun");
+    expect(plugin.schema.parse(item)).toEqual(item);
+  });
+});
